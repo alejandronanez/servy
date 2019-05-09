40d726ce68753cd2e24d93aad0d66a75110de12f
Alejandro Nanez <alejonanez@gmail.com>
2019-05-08 21:58:54 -0500

    Add params to CONV and parsed them

--- lib/servy/conv.ex
+++ lib/servy/conv.ex
@@ -1,5 +1,9 @@
 defmodule Servy.Conv do
-  defstruct method: "", path: "", resp_body: "", status: nil
+  defstruct method: "",
+            params: %{},
+            path: "",
+            resp_body: "",
+            status: nil
 
   def full_status(conv) do
     "#{conv.status} #{status_reason(conv.status)}"
--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -2,12 +2,21 @@ defmodule Servy.Parser do
   alias Servy.Conv
 
   def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first()
-      |> String.split(" ")
+    # Split the request into [...rest, params]
+    [top, params_string] = String.split(request, "\n\n")
 
-    %Conv{method: method, path: path}
+    # Split ...rest into request line, and headers
+    [request_line | header_lines] = String.split(top, "\n")
+
+    # Get method and path from the request
+    [method, path, _] = String.split(request_line, " ")
+
+    params = parse_params(params_string)
+
+    %Conv{method: method, path: path, params: params}
+  end
+
+  def parse_params(params_string) do
+    params_string |> String.trim() |> URI.decode_query()
   end
 end
