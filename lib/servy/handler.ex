90e8be1cf6d7cc9413afbf32888a1232c091e9e2
Alejandro Nanez <alejonanez@gmail.com>
2019-05-08 21:35:08 -0500

    Create struct and add "typesafety" to our code

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,6 +2,7 @@ defmodule Servy.Handler do
   @moduledoc """
   Handles HTTP requests
   """
+  alias Servy.Conv
 
   @pages_path Path.expand("../../pages", __DIR__)
 
@@ -21,42 +22,42 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def route(%{method: "GET", path: "/wildthings"} = conv) do
+  def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(%{method: "GET", path: "/bears"} = conv) do
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
     %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
-  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+  def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
-  def route(%{method: "GET", path: "/about"} = conv) do
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
     @pages_path
     |> Path.join("about.html")
     |> File.read()
     |> handle_file(conv)
   end
 
-  def route(%{path: path} = conv) do
+  def route(%Conv{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here"}
   end
 
-  def handle_file({:ok, content}, conv) do
+  def handle_file({:ok, content}, %Conv{} = conv) do
     %{conv | status: 200, resp_body: content}
   end
 
-  def handle_file({:error, :enoent}, conv) do
+  def handle_file({:error, :enoent}, %Conv{} = conv) do
     %{conv | status: 404, resp_body: "File not found"}
   end
 
-  def handle_file({:error, reason}, conv) do
+  def handle_file({:error, reason}, %Conv{} = conv) do
     %{conv | status: 500, resp_body: "File error: #{reason}"}
   end
 
-  def format_response(conv) do
+  def format_response(%Conv{} = conv) do
     """
     HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
