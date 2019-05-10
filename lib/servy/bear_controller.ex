7e4c5127e084a44257a75108b3a0b9343af08ff1
Alejandro Nanez <alejonanez@gmail.com>
2019-05-09 19:43:26 -0500

    Add templates + format code

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -13,8 +13,7 @@ defmodule Servy.BearController do
       |> Enum.filter(&Bear.is_grizzly/1)
       |> Enum.sort(&Bear.order_asc_by_name/2)
       |> Enum.map(&bear_item/1)
-      |> Enum.join
-
+      |> Enum.join()
 
     %{conv | status: 200, resp_body: "<ul>#{items}</ul>"}
   end
@@ -25,7 +24,7 @@ defmodule Servy.BearController do
     %{conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}</h1>"}
   end
 
-  def create(%Conv{} = conv, %{"name" => name, "type" => type}) do 
+  def create(%Conv{} = conv, %{"name" => name, "type" => type}) do
     %{
       conv
       | status: 201,
