8c31b6953aea576d8b06329132aaa81189a617c9
Alejandro Nanez <alejonanez@gmail.com>
2019-05-10 19:22:05 -0500

    Adding tests!

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -3,10 +3,10 @@ defmodule Servy.Parser do
 
   def parse(request) do
     # Split the request into [...rest, params]
-    [top, params_string] = String.split(request, "\n\n")
+    [top, params_string] = String.split(request, "\r\n\r\n")
 
     # Split ...rest into request line, and headers
-    [request_line | header_lines] = String.split(top, "\n")
+    [request_line | header_lines] = String.split(top, "\r\n")
 
     # Get method and path from the request
     [method, path, _] = String.split(request_line, " ")
@@ -23,6 +23,16 @@ defmodule Servy.Parser do
 
   def parse_params(_, _), do: %{}
 
+  @doc """
+  Parses the given param string of the form `key1=value1&key2=value2`
+
+  ## Examples
+      iex> params_string = "name=Baloo&type=Brown"
+      iex> Servy.Parser.parse_params("application/x-www-form-urlencoded", params_string)
+      %{"name" => "Baloo", "type" => "Brown"}
+      iex> Servy.Parser.parse_params("multipart/form-data", params_string)
+      %{}
+  """
   def parse_headers([head | tail], headers) do
     [key, value] = String.split(head, ": ")
     headers = Map.put(headers, key, value)
